9245bc1de3a0439f03e84dff23a09e6a3e7b6e32
Roman <47541355+RLobodenko@users.noreply.github.com>
2024-08-14 08:21:03 +0300

    Add files via upload

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+useHead ( input: {
+
+
+    script: [
+        {src: 'https://telegram.org/js/telegram-web-app.js', defer: true}
+    ],
+})
+
+<template>
+<div>
+<ClientOnly>
+<slot/>
+</ClientOnly>
+</div>
+
+</template>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,137 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<section>
+<div class="q"><span>Овен (Aries). 21 марта — 19 апреля</span><br><img src="../_1.png"></div>
+<div class="w"><span>Телец (Taurus). 20 апреля — 20 мая</span><br> <img src="../_2.png"></div>
+<div class="e"><span>Близнецы (Gemini). 21 мая — 21 июня </span><br><img src="../_3.png"></div>
+<div class="r"><span>Рак (Cancer). 22 июня — 22 июля</span><br> <img src="../_4.png"></div>
+<div class="t"><span>Лев (Leo). 23 июля — 22 августа </span><br><img src="../_5.png"></div>
+<div class="o"><span>Дева (Virgo). 23 августа — 22 сентября</span><br> <img src="../_6.png"></div>
+<div class="u"><span>Весы (Libra). 23 сентября — 23 октября</span><br> <img src="../_7.png"></div>
+<div class="i"><span>Скорпион (Scorpio). 24 октября — 21 ноября</span><br> <img src="../_8.png"></div>
+<div class="y"><span>Стрелец (Sagittarius). 22 ноября — 21 декабря </span><br><img src="../_9.png"></div>
+<div class="l"><span>Козерог (Capricorn). 22 декабря — 20 января</span><br> <img src="../_10.png"></div>
+<div class="k"><span>Водолей (Aquarius). 21 января — 20 февраля </span><br><img src="../_11.png"></div>
+<div class="m"><span>Рыбы (Pisces). 21 февраля — 20 марта </span><br><img src="../_12.png"></div>
+</section>
+
+</template>
+
+<style>
+span {
+margin-left: 10px;
+}
+
+img {
+height: 50px;
+width: 50px;
+position: absolute;
+margin-left: 10px;
+margin-top: 10px;
+}
+ div {
+	
+	width: 33%;
+	height: 25%;
+	
+}
+
+
+
+.q {
+	position: absolute;
+	left: 0px;
+	top: 0px;
+	border: 1px solid black;
+	
+
+}
+
+.e {
+	position: absolute;
+	top: 0px;
+	left: 66%;
+	border: 1px solid black;
+	
+}
+
+
+.w {
+	
+	position: absolute;
+	top: 0px;
+	left: 33%;
+border: 1px solid black;
+
+	
+}
+.r {
+	position: absolute;
+	left: 0px;
+	top: 25%;
+	border: 1px solid black;
+}
+
+.t {
+	position: absolute;
+	left: 33%;
+	top: 25%;
+border: 1px solid black;
+}
+
+.y {
+	position: absolute;
+	top: 50%;
+	left: 66%;
+	border: 1px solid black;
+}
+
+.u {
+	position: absolute;
+	left: 0px;
+	top: 50%;
+	border: 1px solid black;
+}
+
+
+.i {
+	position: absolute;
+	left: 33%;
+	top: 50%;
+	border: 1px solid black;
+}
+
+
+.o {
+	position: absolute;
+	top: 25%;
+	left: 66%;
+	border: 1px solid black;
+}
+.l {
+	position: absolute;
+	left: 0px;
+	top: 75%;
+	border: 1px solid black;
+}
+
+
+.k {
+	position: absolute;
+	left: 33%;
+	top: 75%;
+	border: 1px solid black;
+}
+
+
+.m {
+	position: absolute;
+	top: 75%;
+	left: 66%;
+	border: 1px solid black;
+}
+
+
+</style>
